cf19cc4eb2b0ed7be4a3bda4e95c6463d059b0f7
JeffryantoW <jeffryanto.wijaya@gmail.com>
2021-01-23 23:28:28 +0700

    Update AdminTemplate.php

--- application/views/template/backend/AdminTemplate.php
+++ application/views/template/backend/AdminTemplate.php
@@ -35,8 +35,8 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/datatables.bootstrap4.min.css') ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/buttons.datatables.min.css') ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/responsive.bootstrap4.min.css') ?>">
-
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css')?>">
+	
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
     <link rel="stylesheet" href="<?php echo base_url('assets/css/waves.min.css')?>" type="text/css" media="all">
 
