0afb3461c1c7fcec65a9bb603268e77272374052
JeffryantoW <jeffryanto.wijaya@gmail.com>
2021-01-23 21:41:29 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -25,9 +25,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 
 // $config['base_url'] = 'http://localhost/sia_sekolah-ci3/';
-$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
-$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = "http://18.216.115.189:8081";
 
 date_default_timezone_set('Asia/Jakarta');
 
