6c138223a491ead1f06d3ebcb7eac1d9f317e542
JeffryantoW <jeffryanto.wijaya@gmail.com>
2021-01-23 23:35:07 +0700

    Update AdminTemplate.php

--- application/views/template/backend/AdminTemplate.php
+++ application/views/template/backend/AdminTemplate.php
@@ -31,12 +31,11 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Quicksand:500,700" rel="stylesheet">
 
-
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/datatables.bootstrap4.min.css') ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/buttons.datatables.min.css') ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/responsive.bootstrap4.min.css') ?>">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.7/css/responsive.bootstrap4.min.css">
 	
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
     <link rel="stylesheet" href="<?php echo base_url('assets/css/waves.min.css')?>" type="text/css" media="all">
 
