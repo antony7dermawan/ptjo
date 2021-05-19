f5aef5eae16a2dc1f3785b933858ab4fa2a9128e
antony7dermawan <62688094+antony7dermawan@users.noreply.github.com>
2021-05-19 13:52:22 +0700

    up

--- application/controllers/C_dashboard.php
+++ application/controllers/C_dashboard.php
@@ -18,14 +18,19 @@ class C_dashboard extends MY_Controller
 
   public function index()
   {
+    $title = "Dashboard";
+    if(date('m-d')=='05-30')
+    {
+      $title = 'Selamat Ulang Tahun Ibu Nata! Sukses untuk PT JPAT';
+    }
     $data = [
       "c_t_po" => $this->m_t_po->select_for_dashboard(),
       "c_setting_db_bank_coa" => $this->m_setting_db_bank_coa->select('2021-01-01','2100-01-01'),
       "c_setting_db_supplier_coa" => $this->m_setting_db_supplier_coa->select('2021-01-01','2100-01-01'),
       "select_no_faktur" => $this->m_t_ak_faktur_penjualan->select_no_faktur(),
       "pengiriman_select" => $this->m_t_t_a_penjualan_pks->select_sum_in_date($this->session->userdata('date_from_dashboard'),$this->session->userdata('date_to_dashboard')),
-      "title" => "Dashboard",
-      "description" => "Web Version:21-01-26 22:11"
+      "title" => $title,
+      "description" => "Web Version:21-05-29 13:11"
     ];
 
     $this->render_backend('template/backend/pages/dashboard', $data);
