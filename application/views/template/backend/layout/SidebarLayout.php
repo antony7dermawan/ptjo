6f3570ce62b98328ba940bf5d9476030cd21fb18
antony7dermawan <62688094+antony7dermawan@users.noreply.github.com>
2021-08-04 00:20:24 +0700

    up

--- application/views/template/backend/layout/SidebarLayout.php
+++ application/views/template/backend/layout/SidebarLayout.php
@@ -470,6 +470,12 @@ if($level_user_id==1 or $level_user_id==6 )
                     <span class="pcoded-mtext">Setting Print Terima Pelanggan</span>
                 </a>
             </li>
+
+            <li class="">
+                <a href="<?= base_url("c_t_ak_pembayaran_supplier_print_setting"); ?>" class="submenu waves-effect waves-dark">
+                    <span class="pcoded-mtext">Setting Print Pembayaran Supplier</span>
+                </a>
+            </li>
         </ul>
     </li>
 
@@ -576,6 +582,15 @@ if($level_user_id==1 or $level_user_id==2 or $level_user_id==4 or $level_user_id
                     </a>
                 </li>
             </ul>
+            <ul class="pcoded-submenu">
+                <li class="">
+                    <a href="<?= base_url("c_t_ak_pembayaran_supplier"); ?>" class="submenu waves-effect waves-dark">
+                        <span class="pcoded-mtext">Pembayaran Supplier</span>
+                    </a>
+                </li>
+            </ul>
+
+
             <?php
         }
 
